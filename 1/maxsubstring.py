509a65daf11765fc14761be6dc51dac8af0f4a2a
kremenovic96 <ranko.kremenovic@gmail.com>
2016-09-14 16:46:41 +0200

    finishing pset1

--- 1/maxsubstring.py
+++ 1/maxsubstring.py
@@ -1,16 +1,18 @@
 s = 'azcbobobegghakl'
 ans0 = ''
 ans1 = ''
-for char in s:
-	if ans1 == '':
-		ans1 += char
-	elif char >= ans1[-1]:
-		ans1 += char
-	elif char < ans1[-1]:
-         ans0 += char
-	if len(ans0) > len(ans1):
-        	ans1 = ''
-
-if len(ans0) > len(ans1):
-	ans1 = ans0
-print(ans1)			
+counter = 0
+for n in range(len(s)):
+    
+    for i in range(n, len(s)-1):
+        if s[i+1] >= s[i]:
+            ans0 += s[i]
+            counter += 1
+        else:
+            break
+    if len(ans0) > len(ans1):
+        ans1 = ans0
+        ans0 = ''
+    n = counter    
+print(ans1)  
+print (len(s))
